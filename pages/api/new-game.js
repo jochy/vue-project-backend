9f9525600b7b4db58ace5efaba2bb59c0abc1909
Joch <joch41@gmail.com>
2022-11-22 12:08:12 +0100

    Add cors support

--- pages/api/new-game.js
+++ pages/api/new-game.js
@@ -1,16 +1,8 @@
 import FrenchWordsLefff from 'french-words-gender-lefff/dist/words.json' assert {type: 'json'};
-import NextCors from 'nextjs-cors';
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 export default async function (req, res) {
-    await NextCors(req, res, {
-        // Options
-        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-        origin: '*',
-        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    });
-
     let words = Object.keys(FrenchWordsLefff)
         // Keep words with 5 chars
         .filter(it => it.length === 5)

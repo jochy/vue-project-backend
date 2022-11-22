c027f6cdac46b185d02f1aa5f167b2b8c7c0fe87
Joch <joch41@gmail.com>
2022-11-22 12:03:17 +0100

    Add cors support

--- pages/api/check-word.js
+++ pages/api/check-word.js
@@ -1,6 +1,14 @@
 import FrenchWordsLefff from 'french-words-gender-lefff/dist/words.json' assert {type: 'json'};
+import NextCors from 'nextjs-cors';
 
 export default async function (req, res) {
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
+
     res.json({
         isWord: FrenchWordsLefff[req.body.word] != null
     });
